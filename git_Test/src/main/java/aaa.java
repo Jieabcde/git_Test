acbf4ff2cbc1f505486f9c510fdaac3bd6772f11
yanjie <1403407797@qq.com>
2022-06-18 19:29:23 +0800

    push commit2

--- git_Test/src/main/java/aaa.java
+++ git_Test/src/main/java/aaa.java
@@ -2,20 +2,19 @@ public class aaa {
     public static void main(String[] args) {
         System.out.println(1313);
         System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
+        System.out.println(1313);
 
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
+
 
     }
 }
