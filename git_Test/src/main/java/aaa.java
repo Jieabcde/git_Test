26fa6752b2b9a9f224c16a4f9dce5cec89d1a8d0
yanjie <1403407797@qq.com>
2022-08-09 05:17:25 +0800

    gitee commit1

--- git_Test/src/main/java/aaa.java
+++ git_Test/src/main/java/aaa.java
@@ -1,19 +1,7 @@
 public class aaa {
     public static void main(String[] args) {
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
-        System.out.println(1313);
 
+        System.out.println(8-9);
 
 
     }
