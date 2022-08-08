98970124103da4053d2482c3692766ce011d6a71
yanjie <1403407797@qq.com>
2022-08-09 05:18:51 +0800

    gitee commit1

--- git_Test/src/main/java/aa.java
+++ git_Test/src/main/java/aa.java
@@ -2,7 +2,5 @@ public class aa {
     public static void main(String[] args) {
         System.out.println(2);
         System.out.println("gitHub_pullaa");
-        System.out.println("Gitee");
-        System.out.println("Gitee");
     }
 }
